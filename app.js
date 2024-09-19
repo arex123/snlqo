dbb9012ba8ecf272e9937ac0777b16b8c0a677ae
arex123 <ad47kumar@gmail.com>
2024-09-19 14:30:38 +0530

    nosql connection

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database')
+const db = require('./util/database').mongodbClient
 const app = express();
 require('dotenv').config()
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-// const adminRoutes = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 // const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,14 +23,15 @@ app.use((req, res, next) => {
   //     next();
   //   })
   //   .catch(err => console.log(err));
+  next()
 });
 
-// app.use('/admin', adminRoutes);
+app.use('/admin', adminRoutes);
 // app.use(shopRoutes);
 
 // app.use(errorController.get404);
 
-db((client)=>{
-  console.log("33")
+db(()=>{
+  // console.log("33")
   app.listen(3001)
 })
--- util/database.js
+++ util/database.js
@@ -1,13 +1,25 @@
 const mongodb = require('mongodb')
 const client = mongodb.MongoClient;
 console.log("db url ",process.env.mongodbURL)
+
+let _db;
 const mongodbClient = callback => {
   client.connect(process.env.mongodbURL).then((res)=>{
     console.log("res" ,res)
-    callback(res)
+    _db = res.db()
+    callback()
   }).catch(err=>{
     console.log("err while connecting db ",err)
+    throw err
   })
 }
 
-module.exports = mongodbClient
+const getDb = ()=>{
+  if(_db){
+    return _db
+  }
+  throw 'No database found'
+}
+
+exports.mongodbClient = mongodbClient
+exports.getDb = getDb
