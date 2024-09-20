38fb2d63a7aa1f6096ecc96dcd93c1d8458eff5e
arex123 <ad47kumar@gmail.com>
2024-09-20 17:15:09 +0530

    user

--- models/user.js
+++ models/user.js
@@ -1,6 +1,27 @@
-const Sequelize = require('sequelize');
+const getDb = require('../util/database').getDb
+const mongodb = require('mongodb')
 
-const sequelize = require('../util/database');
+class User{
+  constructor(name,email,id){
+    this.name = name
+    this.email = email
+    this._id = id
+  }
+
+  insertOne(){
+    let db = getDb()
+   return db.collections('users').insertOne(this)
+  }
+
+  static findUserbyID(id){
+    id = new mongodb.ObjectId(id)
+    let db = getDb()
+    return db.collections('users').find({_id:id}).next()
+
+  }
+
+
+}
 
 const User = sequelize.define('user', {
   id: {
