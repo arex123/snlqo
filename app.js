3982e2952d977b5039ef8e1cc3d2dc0213989fd7
arex123 <ad47kumar@gmail.com>
2024-09-20 23:38:41 +0530

    cart

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const User = require('./models/user')
 const db = require('./util/database').mongodbClient
 const app = express();
 require('dotenv').config()
@@ -13,17 +14,16 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
-  // User.findById(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
-  next()
+  User.findUserbyID('66eda901b20e4e24e9287b5a')
+    .then(user => {
+      req.user = new User(user.name,user.email,user._id,user.cart);
+      next();
+    })
+    .catch(err => console.log(err));
+  // next()
 });
 
 app.use('/admin', adminRoutes);
@@ -33,5 +33,6 @@ app.use(shopRoutes);
 
 db(()=>{
   // console.log("33")
+
   app.listen(3001)
 })
--- models/user.js
+++ models/user.js
@@ -2,36 +2,66 @@ const getDb = require('../util/database').getDb
 const mongodb = require('mongodb')
 
 class User{
-  constructor(name,email,id){
+  constructor(name,email,id,cart){
     this.name = name
     this.email = email
     this._id = id
+    this.cart = cart
   }
 
   insertOne(){
     let db = getDb()
-   return db.collections('users').insertOne(this)
+   return db.collection('users').insertOne(this)
   }
 
   static findUserbyID(id){
     id = new mongodb.ObjectId(id)
     let db = getDb()
-    return db.collections('users').find({_id:id}).next()
+    return db.collection('users').find({_id:id}).next()
 
+  }
+
+  addToCart(product,length=0){
+    console.log("prr ",product)
+    
+    const productPrevIdx = this.cart?.items?.findIndex?.((prod)=>{
+      return prod.productId.toString() === product._id.toString()
+    })
+    let newQt = 1
+    let updatedCartProducts = [...this.cart.items]
+
+    console.log("idx ",productPrevIdx)
+    if(productPrevIdx>=0){
+      // updatedCartProducts = [...this.cart.items]
+      newQt = this.cart.items[productPrevIdx].quantity+1
+      updatedCartProducts[productPrevIdx].quantity = newQt
+    }else{
+      updatedCartProducts.push({
+        productId:new mongodb.ObjectId(product._id),
+        quantity:newQt
+      })
+    }
+    const updatedCart = {
+      items:updatedCartProducts
+    }
+    let db = getDb()
+    return db.collection('users').updateOne({_id:new mongodb.ObjectId(this._id)},{
+      $set:{cart:updatedCart}
+    })
   }
 
 
 }
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+// const User = sequelize.define('user', {
+//   id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     allowNull: false,
+//     primaryKey: true
+//   },
+//   name: Sequelize.STRING,
+//   email: Sequelize.STRING
+// });
 
 module.exports = User;
