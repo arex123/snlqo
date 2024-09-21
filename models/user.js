e6e4240724536525e38d433d2d9ae389896f3fa6
arex123 <ad47kumar@gmail.com>
2024-09-21 11:51:09 +0530

    cart

--- models/user.js
+++ models/user.js
@@ -17,4 +17,30 @@ let userSchema = new Schema({
   },
 });
 
+userSchema.methods.addToCart = function(product){
+  console.log("prr ",product)
+    
+    const productPrevIdx = this.cart?.items?.findIndex?.((prod)=>{
+      return prod.productId.toString() === product._id.toString()
+    })
+    let newQt = 1
+    let updatedCartProducts = [...this.cart.items]
+
+    console.log("idx ",productPrevIdx)
+    if(productPrevIdx>=0){
+      // updatedCartProducts = [...this.cart.items]
+      newQt = this.cart.items[productPrevIdx].quantity+1
+      updatedCartProducts[productPrevIdx].quantity = newQt
+    }else{
+      updatedCartProducts.push({
+        productId:product._id,
+        quantity:newQt
+      })
+    }
+    const updatedCart = {
+      items:updatedCartProducts
+    }
+    this.cart = updatedCart
+    return this.save()
+}
 module.exports = mongoose.model("User", userSchema);
